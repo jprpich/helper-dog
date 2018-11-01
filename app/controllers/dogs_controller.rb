6744fa1a858c91688e754d836fd99cc13fc12dde
Joshua <joshuaprpich@gmail.com>
2018-11-01 09:03:03 -0500

    add dogs index and show

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,14 @@
+class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  private
+    def dog_params
+      params.require(:dog).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  #pages routes
   get '/', to: 'pages#home', as: 'root'
+  get '/about', to: 'pages#about', as: 'about'
+
+  #dogs routes
+  get '/dogs', to: 'dogs#index', as: 'dogs'
+  get '/dogs/:id', to: 'dogs#show', as: 'dog'
 end
